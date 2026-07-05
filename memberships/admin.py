91e82b364ddfab2c3121fe33217a18b9a3c3d8fc
vGabrielGB <vggb106@gmail.com>
2026-07-05 00:11:53 -0400

    Correcciones varias

--- memberships/admin.py
+++ memberships/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Plan, Subscription
 
+admin.site.register(Plan)
+admin.site.register(Subscription)
 # Register your models here.
--- memberships/models.py
+++ memberships/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 
-class Plan(models.fields.Model):
+class Plan(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
